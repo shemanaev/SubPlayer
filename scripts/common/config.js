ec39ca1aadf4c7523205f51d402686ef6e2159bd
Denis Shemanaev <denis@shemanaev.com>
2014-04-30 14:51:56 +0300

    Ya.Dict key added

--- scripts/common/config.js
+++ scripts/common/config.js
@@ -1,7 +1,6 @@
-
 var EXTENSION_NAME = 'SubPlayer'
 var YANDEX_TRANSLATOR_BASE = 'https://dictionary.yandex.net/api/v1/dicservice.json/'
-var YANDEX_TRANSLATOR_KEY = 'KEY_HERE'
+var YANDEX_TRANSLATOR_KEY = 'dict.1.1.20140322T201922Z.9daa20091a9ce7c6.ed15fb781f21df85b09041450845c5ff48d012e2'
 var OS_BASE = 'http://api.opensubtitles.org/xml-rpc'
 var OS_USER_AGENT = 'OS Test User Agent'
 
